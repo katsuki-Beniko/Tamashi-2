71bfbbb840b4bedb2c6a41fca21954d81e96cc80
Heng50 <heng5989.lwh@gmail.com>
2025-08-31 17:47:09 +0800

    animation movement base on direction

--- Assets/Script/camera shake/PlayerController.cs
+++ Assets/Script/camera shake/PlayerController.cs
@@ -1,3 +1,4 @@
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,6 +7,14 @@ public class PlayerController : MonoBehaviour
     public InputAction MoveAction;
     Rigidbody2D rb;
     Vector2 move;
+    private Animator _animator;
+
+    // declare animator parameter
+    public const string _horizontal = "Horizontal";
+    public const string _vertical = "Vertical";
+    public const string _lastHorizontal = "LastHorizontal";
+    public const string _lastVertical = "LastVertical";
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,13 +22,28 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
         MoveAction.Enable();
+
+        // animator define
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
-        Camera.main.GetComponent<CameraController>().Shake(0.5f, 0.3f, 0, true);
+        //Camera.main.GetComponent<CameraController>().Shake(0.5f, 0.3f, 0, true);
+    
+
+        // character moving position
+        _animator.SetFloat(_horizontal, move.x);
+        _animator.SetFloat(_vertical, move.y);
+
+        if (move != Vector2.zero)
+        {
+            _animator.SetFloat(_lastHorizontal, move.x);
+            _animator.SetFloat(_lastVertical, move.y);
+        }
+        // end here
     }
 
     private void FixedUpdate()
